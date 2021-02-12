eea225efac9e3e0e772c99be07d3987b0ef1b093
Erick Malta <erickmalta100@gmail.com>
2021-02-11 23:31:49 -0300

    feat(db): automigrate and connect to db

---
+++ db/db.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ElladanTasartir/go-bank-api/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func InitDatabase(host string, user string, password string, database string, port int) *gorm.DB {
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v", host, user, password, database, port)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal("Failed to connect to database")
+	}
+
+	db.AutoMigrate(&models.Bank{})
+	return db
+}
