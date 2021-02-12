69ff3117ac182087476cfca19546fda112cf27c6
Erick Malta <erickmalta100@gmail.com>
2021-02-11 23:32:08 -0300

    feat(services): add bank-service

---
+++ services/bank-service.go
@@ -0,0 +1,40 @@
+package services
+
+import (
+	"fmt"
+
+	"github.com/ElladanTasartir/go-bank-api/models"
+	"gorm.io/gorm"
+)
+
+type BankService struct {
+	database *gorm.DB
+}
+
+func NewBankService(connection *gorm.DB) *BankService {
+	bankService := &BankService{
+		database: connection,
+	}
+
+	return bankService
+}
+
+func (b *BankService) CreateBank(bank *models.Bank) error {
+	result := b.database.Create(bank)
+	if result.Error != nil {
+		fmt.Printf("Error %v was thrown", result.Error)
+		return result.Error
+	}
+
+	return nil
+}
+
+func (b *BankService) GetBanks(banks *[]models.Bank) error {
+	result := b.database.Find(banks)
+	if result.Error != nil {
+		fmt.Printf("Error %v was thrown", result.Error)
+		return result.Error
+	}
+
+	return nil
+}
