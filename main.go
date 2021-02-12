f334ec60b844bc0217061da22722e14042e15eb8
Erick Malta <erickmalta100@gmail.com>
2021-02-11 23:34:29 -0300

    feat(main): use envs and connect to database

--- main.go
+++ main.go
@@ -4,16 +4,28 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
+	"strconv"
 
 	"github.com/ElladanTasartir/go-bank-api/config"
+	"github.com/ElladanTasartir/go-bank-api/db"
 	"github.com/ElladanTasartir/go-bank-api/routes"
 )
 
 func main() {
-	config.LoadEnv()
-	port := os.Getenv("PORT")
+	envs := config.LoadEnv()
+	port := envs["PORT"]
+	host := envs["POSTGRES_HOST"]
+	user := envs["POSTGRES_USER"]
+	password := envs["POSTGRES_PASSWORD"]
+	database := envs["POSTGRES_DB"]
+	dbPort, err := strconv.Atoi(envs["POSTGRES_PORT"])
+	if err != nil {
+		log.Fatal("Port is not a number")
+	}
+
+	connection := db.InitDatabase(host, user, password, database, dbPort)
+	log.Printf("Database connected")
 	log.Printf("Listening on port %v \n", port)
 	address := fmt.Sprintf(":%v", port)
-	log.Fatal(http.ListenAndServe(address, routes.NewRouter()))
+	log.Fatal(http.ListenAndServe(address, routes.NewRouter(connection)))
 }
