5b14650e2fd89cf9e5e943ce00fb0e3ec0c11b9e
Erick Malta <erickmalta100@gmail.com>
2021-02-11 23:28:12 -0300

    feat(config): create map of envs and load postgres env

--- config/config.go
+++ config/config.go
@@ -1,7 +1,19 @@
 package config
 
-import "github.com/joho/godotenv"
+import (
+	"os"
 
-func LoadEnv() {
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() map[string]string {
 	godotenv.Load()
+	envs := make(map[string]string)
+	envs["PORT"] = os.Getenv("PORT")
+	envs["POSTGRES_HOST"] = os.Getenv("POSTGRES_HOST")
+	envs["POSTGRES_USER"] = os.Getenv("POSTGRES_USER")
+	envs["POSTGRES_PASSWORD"] = os.Getenv("POSTGRES_PASSWORD")
+	envs["POSTGRES_DB"] = os.Getenv("POSTGRES_DB")
+	envs["POSTGRES_PORT"] = os.Getenv("POSTGRES_PORT")
+	return envs
 }
