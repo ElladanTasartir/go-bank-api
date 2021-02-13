1deb50fc4c7c3f5722ba74b2589e3e7fa8304b55
Erick Malta <erickmalta100@gmail.com>
2021-02-12 22:11:41 -0300

    feat(model): use uuid instead of int

--- models/bank.go
+++ models/bank.go
@@ -1,7 +1,11 @@
 package models
 
+import (
+	"github.com/google/uuid"
+)
+
 type Bank struct {
-	ID   int
-	Name string `json:"name"`
-	Code int    `json:"code"`
+	ID   uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()"`
+	Name string    `json:"name"`
+	Code int       `json:"code"`
 }
