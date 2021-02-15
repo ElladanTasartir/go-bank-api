7cff5e0359d6af8a38f1074e5c5d356c32a5dda8
Erick Malta <erickmalta100@gmail.com>
2021-02-14 23:31:48 -0300

    feat(services): add GetBank and DeleteBank

--- services/bank-service.go
+++ services/bank-service.go
@@ -29,8 +29,28 @@ func (b *BankService) CreateBank(bank *models.Bank) error {
 	return nil
 }
 
+func (b *BankService) GetBank(bank *models.Bank, id string) error {
+	result := b.database.Where("id = ?", id).First(bank)
+	if result.Error != nil {
+		fmt.Printf("Error %v was thrown", result.Error)
+		return result.Error
+	}
+
+	return nil
+}
+
 func (b *BankService) GetBanks(banks *[]models.Bank) error {
 	result := b.database.Find(banks)
+	if result.Error != nil {
+		fmt.Printf("Error %v was thrown", result.Error)
+		return result.Error
+	}
+
+	return nil
+}
+
+func (b *BankService) DeleteBanks(bank *models.Bank, id string) error {
+	result := b.database.Where("id = ?", id).Delete(bank)
 	if result.Error != nil {
 		fmt.Printf("Error %v was thrown", result.Error)
 		return result.Error
