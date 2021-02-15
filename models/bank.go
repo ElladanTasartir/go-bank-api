c875d36b3031035bb25bf14326e7bb40b28ab6e8
Erick Malta <erickmalta100@gmail.com>
2021-02-14 23:31:24 -0300

    feat(model): add timestamps

--- models/bank.go
+++ models/bank.go
@@ -2,9 +2,11 @@ package models
 
 import (
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type Bank struct {
+	gorm.Model
 	ID   uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()"`
 	Name string    `json:"name"`
 	Code int       `json:"code"`
