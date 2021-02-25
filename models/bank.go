3ae9772a52053cba21680dec60cf86fa119678b7
Erick Malta <erickmalta100@gmail.com>
2021-02-25 00:34:27 -0300

    feat(bank): use base model

--- models/bank.go
+++ models/bank.go
@@ -1,13 +1,7 @@
 package models
 
-import (
-	"github.com/google/uuid"
-	"gorm.io/gorm"
-)
-
 type Bank struct {
-	gorm.Model
-	ID   uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()"`
-	Name string    `json:"name"`
-	Code int       `json:"code"`
+	Base
+	Name string `json:"name"`
+	Code int    `json:"code"`
 }
